ab84594dee3e3d8d087b9f96ec618729dbe6cfe8
Pekarnik <pekgoglo@gmail.com>
2019-09-28 07:10:49 +0300

    Смог наладить гит, дз загружено

---
+++ Tests/TestWPF/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Net.Mail;
+using System.Net;
+using System.Security;
+using System;
+
+namespace TestWPF
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		
+
+		private void SendButton_OnClick(object sender, RoutedEventArgs e)
+		{
+
+			var emailSender = new EmailSendServiceClass(UserNameEditor.Text,PasswordEditor.SecurePassword);
+			emailSender.SendMessage(NetworkingVariables.host, NetworkingVariables.port, NetworkingVariables.emailFrom, NetworkingVariables.emailTo, NetworkingVariables.name, MessageSubj.Text, Message.Text
+				, NetworkingVariables.successMsg, NetworkingVariables.errorMsg, NetworkingVariables.successCaption, NetworkingVariables.successCaption);
+		}
+	}
+}
